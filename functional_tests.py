6d13ecadb9991cf3f43e8d62f5f2dc9eb596b4bb
trance128 <thesis.17@gmail.com>
2020-04-02 15:25:49 +0100

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -5,7 +5,9 @@ Coded by Ovid Mazuru, following Obey the Testing Goat
 """
 
 import unittest
+import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(unittest.TestCase):
     """
@@ -19,24 +21,41 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         """
-        Name is self explanatory -- test to see if user can start a list and retrieve it later from generated URL
+        Name is self explanatory -- test to see if user can start a list and retrieve
+        it later from generated URL
         """
         # Edith has heard about a cool new To-Do App, she goes to checkout the homepage
         self.browser.get("http://localhost:8000")
 
         # She notices the page title and header mention to-do lists
         self.assertIn("To-Do", self.browser.title)
-        self.fail("Finish the test!")
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # She types 'Buy peacock feathers' in the text box  (She seems to have strange hobbies)
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates and now the page lists
         # '1.  Buy Peacock Feathers' as an item in the to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         # There is still a text-box inviting her to add another item.  She
         # enters "Use Peacock Feathers to make a fly"
+        self.fail("Finish the test!")
 
         # The page updates again, and now shows both items on her list
 
--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,11 @@
+"""
+Standard Django Views
+"""
+
 from django.http import HttpResponse
 
 def home_page(request):
+    """
+    Displays the landing page
+    """
     return HttpResponse('<html><title>To-Do lists</title></html>')
