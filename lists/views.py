37f0a734b5d49a7e27ec08e67ff55022fe1543d7
trance128 <thesis.17@gmail.com>
2020-04-02 18:26:15 +0100

    Adding 2nd test for adding to-do list item

--- lists/views.py
+++ lists/views.py
@@ -1,11 +1,13 @@
 """
 Standard Django Views
 """
-
+from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
     """
     Displays the landing page
     """
-    return render(request, "home.html")
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
