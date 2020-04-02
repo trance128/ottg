68760133997651112798f789ee389346a682c73e
trance128 <thesis.17@gmail.com>
2020-04-02 18:31:52 +0100

    Refactor functional test to use check for row in tables function, rather than repeating our code

--- functional_tests.py
+++ functional_tests.py
@@ -52,7 +52,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        self.check_for_row_in_table("1:  Buy peacock feathers")
+        self.check_for_row_in_table("1: Buy peacock feathers")
 
         # There is still a text-box inviting her to add another item.  She
         # enters "Use Peacock Feathers to make a fly"
@@ -62,7 +62,7 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        self.check_for_row_in_table("1:  Buy peacock feathers")
+        self.check_for_row_in_table("1: Buy peacock feathers")
         self.check_for_row_in_table("2: Use peacock feathers to make a fly")
 
         # Edith wonders whether the site will remember her list.  The she sees that
